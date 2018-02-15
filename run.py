515b86feafc34aa9cf82c7f5dc7a00fab6d6dca9
jfsubrini <jfsubrini@zoho.com>
2018-02-15 07:55:49 +0100

    First commit with some pieces of disparate code

---
+++ run.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+from flask import Flask, render_template, url_for
+from google_api import *
+from mocks import Post
+
+
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+@app.route("/")
+@app.route("/home/")
+def index():
+    # lat = GeoData().coordonates.latitude()
+    # lng = GeoData().coordonates.longitude()
+    # return render_template("home.html", latitude=lat, longitude=lng)
+    posts = Post.all()
+    return render_template("home.html", posts=posts)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
