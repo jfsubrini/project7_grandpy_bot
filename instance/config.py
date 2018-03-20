8f2cb29e694b77e170a3c15a174380e1d9df7212
jfsubrini <jfsubrini@zoho.com>
2018-03-20 21:22:01 +0100

    Deleting instance/*

--- instance/config.py
+++
@@ -1,3 +0,0 @@
-""" Confidential Google Maps API keys. """
-GOOGLE_MAPS_KEY_JS = 'AIzaSyD5VMFZxtVvCob0mp3zYuuzb80_JPyBSCY' # JavaScript API
-GOOGLE_MAPS_KEY_GEOCODING = 'AIzaSyCdrPZF5a3PPvxwXaAq-kTgGun5WEDRj-E' # Geocoding API
