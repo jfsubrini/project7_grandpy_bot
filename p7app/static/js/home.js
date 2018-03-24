bcf0c24101bb6acdecda469dad48f2d56194cff8
jfsubrini <jfsubrini@zoho.com>
2018-03-24 13:44:39 +0100

    Updating with much better jQuery script to manage the messages and the data displayed.

--- p7app/static/js/home.js
+++ p7app/static/js/home.js
@@ -1,5 +1,7 @@
 // jQuery script to display all the messages and the Google map.
 
+// Creating jQuery objects with id from home.html.
+var textUserDiv = $('#textUserDiv');
 var textUser = $('#textUser');
 var address = $('#address');
 var answerAddress = $('#answerAddress');
@@ -12,11 +14,17 @@ var map = $('#map');
 var button = $('#submit');
 var loader = $('#loader');
 
+// Event listener (triggers when clicking the input submit button).
 button.on('click', function(event) {
-  event.preventDefault();
-  // textUser.html('<img src="../static/images/user.png" alt="Icone de l\'utilisateur" title="Icone de l\'utilisateur" width="70" height="70" id="userPic">');
+  event.preventDefault();  
+  textUser.text('');
   address.hide();
+  answerAddress.text('');
+  addressGlobal.text('');
   wikiHistory.hide();
+  answerStory.text('');
+  extractWiki.text('');
+  linkWikipedia.text('');
   map.hide();
   
   $.ajax({
@@ -26,16 +34,57 @@ button.on('click', function(event) {
     type: 'GET',
     success: function(response) {
       textUser.append(response['userText']);
-      textUser.show();
+      textUserDiv.show();
       loader.fadeIn(3500).fadeOut('slow', function() {
         answerAddress.append(response['addressAnswer']);
-        addressGlobal.append(response['globalAddress']);
-        addressGlobal.html('<br>Regarde la jolie carte ci-dessous.');
+        addressGlobal.append(response['globalAddress'] + '<br><br><span class="toMap" id="notIvory"><a href:"#mapPlace">Regarde la jolie carte ci-dessous.</a></span>');
         address.show();
         answerStory.append(response['storyAnswer']);
-        extractWiki.append(response['wikiExtract']); 
-        linkWikipedia.html('<a href="https://fr.wikipedia.org/wiki/openclassrooms">- En savoir plus sur Wikipedia.</a>');
-        wikiHistory.show();
+        if (response['wikiExtract'] != '') {
+          extractWiki.append(response['wikiExtract']); 
+          linkWikipedia.html('<a href="https://fr.wikipedia.org/wiki/openclassrooms">- En savoir plus sur Wikipedia.</a>');
+          wikiHistory.show();
+        }
+        $('input:text').val('')
+        
+
+        // Sending the coordinates of the address to the Google Maps API JavaScript to design and 
+        // display the map with a marker and some other little things.
+        var latitude = response['lat'];
+        var longitude = response['lng'];
+        function initMap() {
+        // Create a map object and specify the DOM element for display.
+        // New map with map options.
+        var userPlace = {lat: latitude, lng: longitude};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: userPlace,
+          zoom: 17,
+          mapTypeId: 'terrain',
+          scaleControl: true,
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, mapTypeIds: ['roadmap', 'hybrid'],
+          }
+        });
+        // Add a symbol (circle) as a marker and animation of the marker.
+          var marker = new google.maps.Marker({
+            position: userPlace,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          icon: { 
+              path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+              scale: 6
+            }
+          // icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+          });
+          // Add an info window that pop up when the mouse mover over.
+          var infoWindow = new google.maps.InfoWindow({
+            content: "<h4>C'est ici !</h4>"
+          });
+          marker.addListener('mouseover', function(){
+            infoWindow.open(map, marker);
+          });
+        }
         map.show();
       });
     },
@@ -43,45 +92,4 @@ button.on('click', function(event) {
       // console.log(error);    // A REVOIR
     }
   });
-
-
-  var latitude = 48.8599614;
-  var longitude = 2.3265614;
-// var latitude = response['lat'];
-// var longitude = response['lng'];
-  function initMap() {
-  // Create a map object and specify the DOM element for display.
-  // New map with map options.
-  var userPlace = {lat: latitude, lng: longitude };
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: userPlace,
-    zoom: 17,
-    mapTypeId: 'terrain',
-    scaleControl: true,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, mapTypeIds: ['roadmap', 'hybrid'],
-    }
-  });
-  // Add a symbol (circle) as a marker and animation of the marker.
-    var marker = new google.maps.Marker({
-      position: userPlace,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    icon: { 
-        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-        scale: 6
-      }
-    // icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-    });
-    // Add an info window that pop up when the mouse mover over.
-    var infoWindow = new google.maps.InfoWindow({
-      content: "<h4>C'est ici !</h4>"
-    });
-    marker.addListener('mouseover', function(){
-      infoWindow.open(map, marker);
-    });
-  }
-
-
 });
