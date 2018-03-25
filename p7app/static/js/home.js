63f52f1b7f809465a6dce616a0f81ab321514582
jfsubrini <jfsubrini@zoho.com>
2018-03-25 21:49:12 +0200

    Changing jQuery and HTML to show the right Wikipedia page when clicking the link for more story about the place.

--- p7app/static/js/home.js
+++ p7app/static/js/home.js
@@ -24,8 +24,7 @@ button.on('click', function(event) {
   wikiHistory.hide();
   answerStory.text('');
   extractWiki.text('');
-  linkWikipedia.hide(); ///////////////////////
-  linkWikipedia.text(''); ////////////////////
+  linkWikipedia.text('');
   map.hide();
   
   $.ajax({
@@ -39,70 +38,69 @@ button.on('click', function(event) {
       loader.fadeIn(3500).fadeOut('slow', function() {
         answerAddress.append(response['addressAnswer']);
         if (response['globalAddress'] != '') {
-          addressGlobal.append(response['globalAddress'] + '<br><br><span class="toMap" id="notIvory"><a href:"#mapPlace">Regarde la jolie carte ci-dessous.</a></span>');
+          	addressGlobal.append(response['globalAddress'] + '<br><br><span class="toMap" id="notIvory"><a href:"#mapPlace">Regarde la jolie carte ci-dessous.</a></span>');
+        
+        //////////
+			// // Sending the coordinates of the address to the Google Maps API JavaScript to design and 
+	  //       // display the map with a marker and some other little things.
+	  //       var latitude = response['lat'];
+	  //       var longitude = response['lng'];
+	  //       function initMap() {
+	  //       // Create a map object and specify the DOM element for display.
+	  //       // New map with map options.
+	  //       var userPlace = {lat: latitude, lng: longitude};
+	  //       var map = new google.maps.Map(document.getElementById('map'), {
+	  //         center: userPlace,
+	  //         zoom: 17,
+	  //         mapTypeId: 'terrain',
+	  //         scaleControl: true,
+	  //         mapTypeControl: true,
+	  //         mapTypeControlOptions: {
+	  //           style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, mapTypeIds: ['roadmap', 'hybrid'],
+	  //         }
+	  //       });
+	  //       // Add a symbol (circle) as a marker and animation of the marker.
+	  //         var marker = new google.maps.Marker({
+	  //           position: userPlace,
+	  //         map: map,
+	  //         animation: google.maps.Animation.DROP,
+	  //         icon: { 
+	  //             path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+	  //             scale: 6
+	  //           }
+	  //         // icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+	  //         });
+	  //         // Add an info window that pop up when the mouse mover over.
+	  //         var infoWindow = new google.maps.InfoWindow({
+	  //           content: "<h4>C'est ici !</h4>"
+	  //         });
+	  //         marker.addListener('mouseover', function(){
+	  //           infoWindow.open(map, marker);
+	  //         });
+	  //       }
+
+        	map.show();
         }
+        ////////////////////
         address.show();
         answerStory.append(response['storyAnswer']);
         if (response['wikiExtract'] != '') {
           extractWiki.append(response['wikiExtract']);
-          linkWikipedia.append('<a href="https://fr.wikipedia.org/wiki/' + response['pageid'] + '>- En savoir plus sur Wikipedia.</a>');
+          linkWikipedia.append('<a href="https://fr.wikipedia.org/wiki/?curid=' + response['pageid'] + '">- En savoir plus sur Wikipedia.</a>');
           wikiHistory.show();
-          linkWikipedia.show();   //////////////////////////////
         } else {
           if (response['globalAddress'] != '') {
             wikiHistory.show();
           }
         }
         $('input:text').val('')
-        ////////
-        map.show();
+
+
       });
     },
     error: function(error) {
       // console.log(error);    // A REVOIR
     }
   });
-
-//////////////////////////////////////////////////////////////////////////////
-
-      // Sending the coordinates of the address to the Google Maps API JavaScript to design and 
-        // display the map with a marker and some other little things.
-        // var latitude = response['lat'];
-        // var longitude = response['lng'];
-        function initMap() {
-        // Create a map object and specify the DOM element for display.
-        // New map with map options.
-        var userPlace = {lat: 36.7186933, lng: -4.4127521};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: userPlace,
-          zoom: 17,
-          mapTypeId: 'terrain',
-          scaleControl: true,
-          mapTypeControl: true,
-          mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, mapTypeIds: ['roadmap', 'hybrid'],
-          }
-        });
-        // Add a symbol (circle) as a marker and animation of the marker.
-          var marker = new google.maps.Marker({
-            position: userPlace,
-          map: map,
-          animation: google.maps.Animation.DROP,
-          icon: { 
-              path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-              scale: 6
-            }
-          // icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-          });
-          // Add an info window that pop up when the mouse mover over.
-          var infoWindow = new google.maps.InfoWindow({
-            content: "<h4>C'est ici !</h4>"
-          });
-          marker.addListener('mouseover', function(){
-            infoWindow.open(map, marker);
-          });
-        }
-
-
 
 });
